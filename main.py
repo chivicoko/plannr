b349a373e197a8ef9321768a486d6db250cd81f6
chivicoko <victor.c.okoye@gmail.com>
2024-04-24 04:41:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi("main.ui", self)
+        self.setWindowTitle("My Daily Tasks")
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec())
+    
